3fec2a07a2c85f4b944a394be8e8c12badf6b615
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-08-04 16:58:30 -0400

    initial commit

---
+++ PythonProjects/ss/python/one/Assignment_1_2.py
@@ -0,0 +1,23 @@
+'''
+Created on Aug 4, 2021
+
+@author: Wyatt Meehan
+'''
+
+if __name__ == '__main__':
+    
+    # 1) 
+    
+    
+    # 2)
+    
+    
+    # 3)
+    
+    print('Hello World')
+    print('Hello World: 10')
+    
+    # 4)
+    
+    
+    
