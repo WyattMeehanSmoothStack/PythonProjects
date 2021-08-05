6917c4b232da94b29424beeed2a1abf97b013ec7
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-08-05 13:18:44 -0400

    Day_2_a done

--- PythonProjects/ss/python/one/Assignment_1_2.py
+++ PythonProjects/ss/python/one/Assignment_1_2.py
@@ -7,17 +7,51 @@ Created on Aug 4, 2021
 if __name__ == '__main__':
     
     # 1) 
-    
+    sumExample = 50 + 50
+    print(sumExample)
+    diffExample = 100 - 10
+    print(diffExample, '\n')
     
     # 2)
     
+    test = 6 ^ 6
+    print(test)
+    
+    exponent = 6 ** 6
+    print(exponent)
+    
+    add = 6 + 6 + 6 + 6 + 6
+    print(add , '\n')
     
     # 3)
     
     print('Hello World')
-    print('Hello World: 10')
+    print('Hello World: 10\n')
     
     # 4)
+    
+    p = float(input("Please enter the loan size:\n"))
+    y = float(input("Please enter the interest rate in percentage:\n"))
+    n = float(input("Please enter the number of n:\n"))
+    
+    print('\nInput data ===========================\nLoan: {a} Interest rate: {b}% Months: {c}'
+          .format(a = p, b = y, c = n))
+    
+    r = y/100/12
+    
+    monthly_payment = p * (r * (1+r) ** n)/((1+r)**n-1)
+    
+   
+    print("\nJoel should pay ${0:0.0f} per month!".format(monthly_payment))
+    
+
+        
+        
+         
+        
+        
+    
+    
     
     
     
---
+++ PythonProjects/ss/python/two/Day_2_a.py
@@ -0,0 +1,58 @@
+'''
+Created on Aug 5, 2021
+
+@author: Wyatt Meehan
+'''
+import string
+
+if __name__ == '__main__':
+    
+    # 1) 
+    print("Hello World"[8])
+    
+    # 2)
+    print("Thinker"[2:5])
+    s = "hello"
+    print(s[1])
+    
+    # 3)
+    s = "Sammy"
+    print(s[2:])
+    
+    # 4) 
+    print("".join(set("Mississippi")))
+    
+    # 5)
+    while True:
+        try:
+            i = int(input("Please enter the number of phrases to enter: "))
+            break
+        except ValueError:
+            print("Invalid input, please try again")
+    
+    pal_list = []
+    for x in range(1, i + 1):
+        ques = "Please enter phrase number %s\n" % (x)
+        pal_list.append(input(ques))
+    
+
+   
+    
+    def reverse(stuff_list):
+        ans_list = []
+        for thing in stuff_list:
+            ans_list.append(thing.upper().translate(str.maketrans('', '', string.punctuation))
+                             == thing[::-1].upper().translate(str.maketrans('', '', string.punctuation)))
+        return ans_list    
+            
+    ans_list = (reverse(pal_list))    
+    print("\n")
+    for ans in ans_list:
+        if ans:
+            print('Y', end =" ")
+        else:
+            print('N', end =" ")
+    
+    
+        
+    
