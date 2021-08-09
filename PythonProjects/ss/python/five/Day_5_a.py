303b6baaf700feb2f5f4aa2ab8822bd38ec3014a
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-08-09 13:34:01 -0400

    finished day5a

---
+++ PythonProjects/ss/python/five/Day_5_a.py
@@ -0,0 +1,57 @@
+'''
+Created on Aug 9, 2021
+
+@author: Wyatt Meehan
+'''
+
+
+def get_number():
+    while True:
+        try:
+            number = int(input("\nPlease enter the number of BMI's you would like to calculate (5 max):\n"))
+            if number <= 0 or number > 5:
+                print("Invalid input, please try again\n")
+                continue
+            return number
+        except ValueError:
+            print("Invalid input, please try again\n")
+            continue        
+
+            
+def get_bmis(num):
+    bmis = []
+    for x in range(0, num):
+        while True:
+            try:
+                ques = "\nPlease enter the weight (kg) and height (m) separated by a space (entry {}/{}):\n".format(x + 1, num)
+                weight, height = input(ques).split(" ")
+                weight = float(weight)
+                height = float(height)
+                bmis.append(weight / (height) ** 2)
+                break
+            except ValueError:
+                print("Invalid input, please try again\n")
+        
+    return bmis    
+
+
+def get_grades(bmis):
+    grades = []
+    for bmi in bmis:
+        if bmi >= 30.0:
+            grades.append("obesity")
+            print("obesity", end=" ")
+        elif bmi >= 25.5:
+            grades.append("overweight")
+            print("overweight", end=" ")
+        elif bmi >= 18.5:
+            grades.append("normal")
+            print("normal", end=" ")
+        else:
+            grades.append("udnerweight")
+            print("underweight", end=" ")
+
+       
+if __name__ == '__main__':
+    get_grades(get_bmis(get_number()))
+    
