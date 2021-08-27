81f88679c385d0287f015c3381f181dbffc6775e
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-08-26 22:08:48 -0400

    I love SQL so much

--- PythonProjects/ss/pyspark/FirstSQLPyspark.py
+++ PythonProjects/ss/pyspark/FirstSQLPyspark.py
@@ -1,2 +1,18 @@
+from pyspark.sql import Row
+from pyspark.sql import SparkSession
+
+
+def mapper(line):
+    fields = line.split(",")
+    return  Row(ID=int(fields[0]), cost=float(fields[2]))
+
+
 if __name__ == '__main__':
-    pass
+    spark = SparkSession.builder.appName("SparkSQL").getOrCreate()
+    lines = spark.sparkContext.textFile("C:/Users/meeha/OneDrive/Desktop/SmoothStack/Data/customer-orders.csv")
+    transactions = lines.map(mapper)
+    schema_cus = spark.createDataFrame(transactions).cache()
+    schema_cus.createOrReplaceTempView("transactions")
+    total_costs = spark.sql("Select ID, sum(cost) From transactions Group By ID Order By ID")
+    for cus_id, cost in total_costs.collect():
+        print("CustomerId: {} Total Bill: {:0.2f}".format(cus_id, cost))
